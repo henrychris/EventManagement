5889fcc0d6d83471ee1dde2556859b774b826ef2
henrychris <64085210+henrychris@users.noreply.github.com>
2023-10-17 18:00:45 +0100

    add event Errors

---
+++ EventModule/ServiceErrors/Errors.Events.cs
@@ -0,0 +1,13 @@
+﻿using ErrorOr;
+
+namespace EventModule.ServiceErrors;
+
+public static class Errors
+{
+    public static class Event
+    {
+        public static Error NotFound => Error.NotFound(
+            code: "Event.NotFound",
+            description: "Event not found");
+    }
+}
