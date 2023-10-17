0a0be098db3e6d0da10251e8e5ed349972a5cac8
henrychris <64085210+henrychris@users.noreply.github.com>
2023-10-17 17:59:07 +0100

    add api response objects

---
+++ Shared/Responses/ApiErrorResponse.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+
+namespace Shared.Responses;
+
+public class ApiErrorResponse<T>
+{
+    public ApiErrorResponse(T errors, string message)
+    {
+        Errors = errors;
+        Message = message;
+    }
+
+    public bool Status { get; set; }
+    public string Message { get; set; }
+    public T Errors { get; set; }
+
+    public string ToJsonString()
+    {
+        var options = new JsonSerializerOptions
+        {
+            WriteIndented = true
+        };
+        return JsonSerializer.Serialize(this, options);
+    }
+}
---
+++ Shared/Responses/ApiResponse.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+
+namespace Shared.Responses;
+
+public class ApiResponse<T>
+{
+    public ApiResponse(T? data, string message, bool status)
+    {
+        Data = data;
+        Message = message;
+        Status = status;
+    }
+
+    public bool Status { get; set; }
+    public string Message { get; set; }
+    public T? Data { get; set; }
+
+    public string ToJsonString()
+    {
+        var options = new JsonSerializerOptions
+        {
+            WriteIndented = true
+        };
+        return JsonSerializer.Serialize(this, options);
+    }
+}
