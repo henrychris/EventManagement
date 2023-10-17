9527f3de54cf186d87f1db2dcf5bc72d16537da3
henrychris <64085210+henrychris@users.noreply.github.com>
2023-10-17 18:00:32 +0100

    add ErrorOr extensionMethods

---
+++ Shared/Extensions/ErrorOrExtensions.cs
@@ -0,0 +1,17 @@
+﻿using ErrorOr;
+using Shared.Responses;
+
+namespace Shared.Extensions;
+
+public static class ErrorOrExtensions
+{
+    public static ApiResponse<T> ToSuccessfulApiResponse<T>(this ErrorOr<T> errorOr)
+    {
+        return new ApiResponse<T>(data: errorOr.Value, message: "Success", status: true);
+    }
+
+    public static ApiResponse<T> ToSuccessfulApiResponse<T>(this ErrorOr<T> errorOr, string message)
+    {
+        return new ApiResponse<T>(data: errorOr.Value, message: message, status: true);
+    }
+}
