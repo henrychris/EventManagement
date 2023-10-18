e8ab74aaa6241ae25033f98118c0281dacfa5808
henrychris <64085210+henrychris@users.noreply.github.com>
2023-10-19 00:13:15 +0100

    use normal constructor instead

--- Shared/UserModels/Requests/LoginRequest.cs
+++ Shared/UserModels/Requests/LoginRequest.cs
@@ -2,4 +2,16 @@
 
 namespace Shared.UserModels.Requests;
 
-public record LoginRequest([EmailAddress] string EmailAddress, string Password);
+public record LoginRequest
+{
+    public LoginRequest(string emailAddress, string password)
+    {
+        EmailAddress = emailAddress;
+        Password = password;
+    }
+
+    [DataType(DataType.EmailAddress)]
+    public string EmailAddress { get; init; }
+
+    [DataType(DataType.Password)] public string Password { get; init; }
+}
--- Shared/UserModels/Requests/RegisterRequest.cs
+++ Shared/UserModels/Requests/RegisterRequest.cs
@@ -2,12 +2,23 @@
 
 namespace Shared.UserModels.Requests;
 
-public record RegisterRequest(
-    [Required, StringLength(50)]
-    string FirstName,
-    [Required, StringLength(50)]
-    string LastName,
-    [Required, DataType(DataType.EmailAddress)]
-    string EmailAddress,
-    string Password,
-    string Role);
+public record RegisterRequest
+{
+    public RegisterRequest(string firstName, string lastName, string emailAddress, string password, string role)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        EmailAddress = emailAddress;
+        Password = password;
+        Role = role;
+    }
+
+    public string FirstName { get; init; }
+    public string LastName { get; init; }
+
+    [DataType(DataType.EmailAddress)]
+    public string EmailAddress { get; init; }
+
+    [DataType(DataType.Password)] public string Password { get; init; }
+    public string Role { get; init; }
+}
