83371b4e61f1371441270a5b84c31f72499b3370
henrychris <64085210+henrychris@users.noreply.github.com>
2023-10-19 00:58:16 +0100

    add validationResult extension method
    used to fetch errors from validationResult

---
+++ UserModule/Extensions/ValidationResultExtensions.cs
@@ -0,0 +1,13 @@
+﻿using ErrorOr;
+using FluentValidation.Results;
+
+namespace UserModule.Extensions;
+
+public static class ValidationResultExtensions
+{
+    public static List<Error> ToErrorList(this ValidationResult validationResult)
+    {
+        return validationResult.Errors.Select(x => Error.Validation(x.ErrorCode, x.ErrorMessage))
+            .ToList();
+    }
+}
