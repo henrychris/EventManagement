ab19e433530d0f212222aeef057a88e786ad1306
henrychris <64085210+henrychris@users.noreply.github.com>
2023-10-23 23:00:49 +0100

    add generic repository

---
+++ Shared/Repositories/Implementations/BaseRepository.cs
@@ -0,0 +1,70 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Shared.Repositories.Interfaces;
+
+namespace Shared.Repositories.Implementations;
+
+public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
+{
+    protected readonly DbContext Context;
+
+    protected BaseRepository(DbContext context)
+    {
+        Context = context;
+    }
+
+    public async Task<TEntity?> GetByIdAsync(string id)
+    {
+        return await Context.Set<TEntity>().FindAsync(id);
+    }
+
+    public async Task<IEnumerable<TEntity>> GetAllAsync()
+    {
+        return await Context.Set<TEntity>().ToListAsync();
+    }
+
+    public async Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate)
+    {
+        return await Context.Set<TEntity>().Where(predicate).ToListAsync();
+    }
+
+    public async Task AddAsync(TEntity entity)
+    {
+        await Context.Set<TEntity>().AddAsync(entity);
+    }
+
+    public async Task AddRangeAsync(IEnumerable<TEntity> entities)
+    {
+        await Context.Set<TEntity>().AddRangeAsync(entities);
+    }
+
+    public async Task RemoveByIdAsync(string id)
+    {
+        var entity = await Context.Set<TEntity>().FindAsync(id);
+        if (entity is not null)
+        {
+            Context.Remove(entity);
+        }
+    }
+
+    public void Remove(TEntity entity)
+    {
+        Context.Set<TEntity>().Remove(entity);
+    }
+
+    public void RemoveRangeAsync(IEnumerable<TEntity> entities)
+    {
+        Context.Set<TEntity>().RemoveRange(entities);
+    }
+
+    public async Task<bool> Exists(string id)
+    {
+        var entity = await Context.Set<TEntity>().FindAsync(id);
+        return entity is null;
+    }
+
+    public void Update(TEntity entity)
+    {
+        Context.Set<TEntity>().Entry(entity).State = EntityState.Modified;
+    }
+}
---
+++ Shared/Repositories/Interfaces/IBaseRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+
+namespace Shared.Repositories.Interfaces;
+
+public interface IBaseRepository<TEntity> where TEntity : class
+{
+    Task<TEntity?> GetByIdAsync(string id);
+    Task<IEnumerable<TEntity>> GetAllAsync();
+    Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate);
+
+    Task AddAsync(TEntity entity);
+    Task AddRangeAsync(IEnumerable<TEntity> entities);
+
+    Task RemoveByIdAsync(string id);
+    void Remove(TEntity entity);
+    void RemoveRangeAsync(IEnumerable<TEntity> entities);
+
+    Task<bool> Exists(string id);
+    void Update(TEntity entity);
+}
