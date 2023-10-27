fbe7e87673947c16eb662c4df42a06e174a4df12
henrychris <64085210+henrychris@users.noreply.github.com>
2023-10-27 21:31:15 +0100

    restructure db seeding
    - fix issue where migrate was called on in-memory database
    - separate concerns and make code a bit tidier

--- EventModule/Extensions/EventAppExtensions.cs
+++ EventModule/Extensions/EventAppExtensions.cs
@@ -7,34 +7,63 @@ namespace EventModule.Extensions;
 
 public static class EventAppExtensions
 {
+    private const string DefaultEventName = "Henry's Main Event!";
+    private const string DefaultEventId = "cecb7257-6764-4a5c-a9f8-6412d158214a";
+    private const string InMemoryProviderName = "Microsoft.EntityFrameworkCore.InMemory";
+
     public static async Task SeedDatabase(this WebApplication app)
     {
-        if (app.Environment.IsDevelopment())
+        using var scope = app.Services.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<EventDbContext>();
+
+        Console.WriteLine("EventModule: database seeding starting.");
+        if (IsInMemoryDatabase(context))
         {
-            Console.WriteLine("Starting EventModule database seeding.");
-            using var scope = app.Services.CreateScope();
-
-            var context = scope.ServiceProvider.GetRequiredService<EventDbContext>();
-            await context.Database.EnsureDeletedAsync();
-            await context.Database.MigrateAsync();
-
-            if (!await context.Events.AnyAsync(x => x.Name == "Henry's Main Event!"))
-            {
-                await context.Events.AddAsync(new Event
-                {
-                    Id = "cecb7257-6764-4a5c-a9f8-6412d158214a",
-                    Name = "Henry's Main Event!",
-                    Description = "An event that exists by default.",
-                    Price = 0.00m,
-                    EventStatus = EventStatus.Upcoming,
-                    Date = DateTime.Now.AddDays(1).Date,
-                    StartTime = DateTime.Now.AddDays(1).AddHours(1).Date,
-                    EndTime = DateTime.Now.AddDays(1).AddHours(6).Date
-                });
-            }
-
-            await context.SaveChangesAsync();
-            Console.WriteLine("EventModule database seeding complete.");
+            await SeedInMemoryDatabase(context);
         }
+        else
+        {
+            await MigrateAndSeedDevelopmentDatabase(context);
+        }
+
+        Console.WriteLine("EventModule: database seeding complete.");
+    }
+
+    private static async Task SeedInMemoryDatabase(EventDbContext context)
+    {
+        await context.Database.EnsureDeletedAsync();
+        await context.Database.EnsureCreatedAsync();
+        await SeedEvents(context);
+    }
+
+    private static async Task MigrateAndSeedDevelopmentDatabase(EventDbContext context)
+    {
+        await context.Database.MigrateAsync();
+        await SeedEvents(context);
+    }
+
+    private static async Task SeedEvents(EventDbContext context)
+    {
+        if (!await context.Events.AnyAsync(x => x.Name == DefaultEventName))
+        {
+            await context.Events.AddAsync(new Event
+            {
+                Id = DefaultEventId,
+                Name = DefaultEventName,
+                Description = "An event that exists by default.",
+                Price = 0.00m,
+                EventStatus = EventStatus.Upcoming,
+                Date = DateTime.Now.AddDays(1).Date,
+                StartTime = DateTime.Now.AddDays(1).AddHours(1).Date,
+                EndTime = DateTime.Now.AddDays(1).AddHours(6).Date
+            });
+            
+            Console.WriteLine($"EventModule: Added default event: {DefaultEventName}");
+        }
+    }
+
+    private static bool IsInMemoryDatabase(DbContext context)
+    {
+        return context.Database.ProviderName == InMemoryProviderName;
     }
 }
--- UserModule/Extensions/UserAppExtensions.cs
+++ UserModule/Extensions/UserAppExtensions.cs
@@ -8,25 +8,35 @@ namespace UserModule.Extensions;
 
 public static class UserAppExtensions
 {
+    private const string InMemoryProviderName = "Microsoft.EntityFrameworkCore.InMemory";
+
     public static async Task SeedDatabase(this WebApplication app)
     {
-        if (app.Environment.IsDevelopment())
+        using var scope = app.Services.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<UserDbContext>();
+        await context.Database.EnsureDeletedAsync();
+
+        var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+        var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+        if (IsInMemoryDatabase(context))
         {
-            Console.WriteLine("Starting UserModule database seeding.");
-            using var scope = app.Services.CreateScope();
-
-            var context = scope.ServiceProvider.GetRequiredService<UserDbContext>();
-            await context.Database.EnsureDeletedAsync();
+            await context.Database.EnsureCreatedAsync();
+            await SeedRoles(roleManager);
+        }
+        else
+        {
             await context.Database.MigrateAsync();
-
-            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
-            var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-
             await SeedRoles(roleManager);
             await SeedUsers(userManager);
-            await context.SaveChangesAsync();
-            Console.WriteLine("UserModule database seeding complete.");
         }
+
+        Console.WriteLine("UserModule: database seeding complete.");
+    }
+
+    private static bool IsInMemoryDatabase(DbContext context)
+    {
+        return context.Database.ProviderName == InMemoryProviderName;
     }
 
     private static async Task SeedRoles(RoleManager<IdentityRole> roleManager)
@@ -46,42 +56,34 @@ public static class UserAppExtensions
 
     private static async Task SeedUsers(UserManager<ApplicationUser> userManager)
     {
-        var adminUser = new ApplicationUser
-        {
-            FirstName = "Henry",
-            LastName = "Ihenacho",
-            Email = "xxxx@example.com",
-            NormalizedEmail = "XXXX@EXAMPLE.COM",
-            UserName = "Owner",
-            NormalizedUserName = "OWNER",
-            PhoneNumber = "+111111111111",
-            EmailConfirmed = true,
-            PhoneNumberConfirmed = true,
-            SecurityStamp = Guid.NewGuid().ToString("D")
-        };
+        var adminUser = CreateUser("Owner", "xxxx@example.com", UserRoles.Admin.ToString());
+        var normalUser = CreateUser("User", "user@example.com", UserRoles.User.ToString());
 
-        var normalUser = new ApplicationUser
-        {
-            FirstName = "User",
-            LastName = "Ihenacho",
-            Email = "user@example.com",
-            NormalizedEmail = "USER@EXAMPLE.COM",
-            UserName = "User",
-            NormalizedUserName = "USER",
-            PhoneNumber = "+222222222",
-            EmailConfirmed = true,
-            PhoneNumberConfirmed = true,
-            SecurityStamp = Guid.NewGuid().ToString("D")
-        };
+        await AddUser(userManager, adminUser, "secretPassword12@");
+        await AddUser(userManager, normalUser, "secretPassword12@");
 
-
-        await AddUser(userManager, adminUser, UserRoles.Admin.ToString());
-        await AddUser(userManager, normalUser, UserRoles.User.ToString());
         Console.WriteLine("UserModule: User seeding complete.");
     }
 
-    private static async Task AddUser(UserManager<ApplicationUser> userManager,
-        ApplicationUser user, string role)
+    private static ApplicationUser CreateUser(string userName, string email, string role)
+    {
+        return new ApplicationUser
+        {
+            FirstName = userName,
+            LastName = "Ihenacho",
+            Email = email,
+            NormalizedEmail = email.ToUpper(),
+            UserName = userName,
+            NormalizedUserName = userName.ToUpper(),
+            PhoneNumber = $"+1{userName.Length}1".PadLeft(12, '1'),
+            EmailConfirmed = true,
+            PhoneNumberConfirmed = true,
+            SecurityStamp = Guid.NewGuid().ToString("D"),
+            Role = role
+        };
+    }
+
+    private static async Task AddUser(UserManager<ApplicationUser> userManager, ApplicationUser user, string password)
     {
         if (user is null)
         {
@@ -90,12 +92,12 @@ public static class UserAppExtensions
 
         if (await userManager.FindByEmailAsync(user.Email!) is null)
         {
-            var password = new PasswordHasher<ApplicationUser>();
-            var hashed = password.HashPassword(user, "secretPassword12@");
-            user.PasswordHash = hashed;
-            user.Role = role;
+            var passwordHasher = new PasswordHasher<ApplicationUser>();
+            var hashedPassword = passwordHasher.HashPassword(user, password);
+            user.PasswordHash = hashedPassword;
+
             await userManager.CreateAsync(user);
-            await userManager.AddToRoleAsync(user, role);
+            await userManager.AddToRoleAsync(user, user.Role);
         }
     }
 }
