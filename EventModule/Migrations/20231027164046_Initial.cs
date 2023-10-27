870d655aece1e0c9e444853bb9df2a53d00d4bca
henrychris <64085210+henrychris@users.noreply.github.com>
2023-10-27 18:04:12 +0100

    add init migration code for eventModule

--- EventModule/Migrations/20231027164046_Initial.cs
+++ EventModule/Migrations/20231027164046_Initial.cs
@@ -11,19 +11,25 @@ namespace EventModule.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<int>(
-                name: "TicketsAvailable",
-                table: "Events",
-                type: "int",
-                nullable: false,
-                defaultValue: 1);
-            
-            migrationBuilder.AddColumn<int>(
-                name: "TicketsSold",
-                table: "Events",
-                type: "int",
-                nullable: false,
-                defaultValue: 0);
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    StartTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EventStatus = table.Column<int>(type: "int", nullable: false),
+                    TicketsAvailable = table.Column<int>(type: "int", nullable: false, defaultValue: 1),
+                    TicketsSold = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.Id);
+                });
         }
 
         /// <inheritdoc />
