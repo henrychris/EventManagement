9dc46bfed1b5a2b1755fb99e88ef929ef1dab075
henrychris <64085210+henrychris@users.noreply.github.com>
2023-10-28 14:32:00 +0100

    remove tokentest endpoint

--- UserModule/Controllers/AuthController.cs
+++ UserModule/Controllers/AuthController.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Shared.API;
 using Shared.Extensions;
-using Shared.Responses;
 using Shared.UserModels.Requests;
 using UserModule.Interfaces;
 
@@ -12,20 +11,10 @@ namespace UserModule.Controllers;
 public class AuthController : BaseController
 {
     private readonly IAuthenticationService _authenticationService;
-    private readonly ITokenService _tokenService;
 
-    public AuthController(IAuthenticationService authenticationService, ITokenService tokenService)
+    public AuthController(IAuthenticationService authenticationService)
     {
         _authenticationService = authenticationService;
-        _tokenService = tokenService;
-    }
-
-    [AllowAnonymous]
-    [HttpPost("tokenTest")]
-    public IActionResult CreateToken(string emailAddress, string role, string userId)
-    {
-        var token = _tokenService.CreateUserJwt(emailAddress, role, userId);
-        return Ok(new ApiResponse<string>(token, "Success", true));
     }
 
     [AllowAnonymous]
